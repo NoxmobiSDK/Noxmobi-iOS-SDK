b808249bae4484b3818d01cdbb26e371f1e0ba98
MonkeySir <MonkeySir1221@gmail.com>
2023-12-15 11:10:02 +0800

    v2.29.1

--- Noxmobi.framework/Headers/NoxConnectorProtocol.h
+++ Noxmobi.framework/Headers/NoxConnectorProtocol.h
@@ -22,6 +22,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (NSString *)apiToken;
 - (NSString *)placementID;
 - (UIViewController * __nullable)rootViewController;
+- (NoxPrivacy *)adPrivacy;
 
 - (BOOL)mutedStart;
 - (BOOL)testModeEnable;
